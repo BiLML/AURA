424ecd6a7558c828b8349c5e8a7f625c4a832938
ZojilamIT <manhmomatic@gmail.com>
2026-01-31 16:02:31 +0700

    fixx main.tsx
    j

--- aura-frontend/src/main.tsx
+++ aura-frontend/src/main.tsx
@@ -6,7 +6,8 @@ import { GoogleOAuthProvider } from '@react-oauth/google';
 
 // 2. Dán mã Client ID bạn vừa lấy được từ Google Cloud vào đây
 // Ví dụ: "123456789-abcdef.apps.googleusercontent.com"
-const CLIENT_ID = "413884508202-hmq7so5tjhnu9hib3vr9pruhqo1utlo3.apps.googleusercontent.com"; 
+// Dùng đúng ID từ Google Cloud Console của bạn
+const CLIENT_ID = "117647364092-9a25olu0rsmc5betuu4k95hijctgo37g.apps.googleusercontent.com";
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
